488996280b0e1979d85ca90f1e2e94e40bce9a65
rexim <reximkut@gmail.com>
2021-04-11 22:22:49 +0700

    Make the background black

--- index.js
+++ index.js
@@ -138,7 +138,7 @@ varying vec2 position;
 
 void main() {
     vec2 coord = position * resolution;
-    gl_FragColor = vec4(0.25, 0.0, 0.0, 1.0);
+    gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
 }
 `
 
